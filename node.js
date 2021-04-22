a12906fcf7f6d2d84ab437cd87e5b779c416a3eb
Kevin <kevin860804@gmail.com>
2021-04-22 18:32:27 +0800

    second

--- node.js
+++ node.js
@@ -1,5 +1,13 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block{
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
